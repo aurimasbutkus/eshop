9436cdf3475ca486a986e26a1b0083c9ec12dd6d
aurimasbutkus <aurimas.butkus3@gmail.com>
2018-11-13 11:22:26 +0200

    Add product factory and product table seeder

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['title', 'price', 'description', 'specification', 'config', 'category', 'quantity', 'status'];
 }
