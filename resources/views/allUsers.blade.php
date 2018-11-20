ac03419f24ec922beaf57d02278ab09692db145b
daumenas <daumius@gmail.com>
2018-11-20 11:57:12 +0200

    Add - added search function for "Display users"

--- resources/views/allUsers.blade.php
+++ resources/views/allUsers.blade.php
@@ -1,54 +1,72 @@
 @extends('layouts.app')
 @section('content')
-        <div class="form-group" >
-            <label for="search">Find Users:</label>
-            <input type="search" class="form-control" id="search" name="search">
-            <button type="search" class="btn btn-default">Search</button>
-        </div>
 
-<table class="table table-hover">
+    <div class="input-group input-group-lg">
+        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+    </div>
 
-    <thead>
+        <table id="myTable" class="table table-hover">
+            <thead>
 
-    <th>Name</th>
+            <th>Name</th>
 
-    <th>Last Name</th>
+            <th>Last Name</th>
 
-    <th>Email</th>
+            <th>Email</th>
 
-    <th>Phone</th>
+            <th>Phone</th>
 
-    <th>Address</th>
+            <th>Address</th>
 
-    <th>Country</th>
+            <th>Country</th>
 
-    </thead>
+            </thead>
 
-    <tbody>
-    @foreach($users as $user)
+            <tbody>
+            @foreach($users as $user)
 
-        <tr>
+                <tr>
 
-            <td>{{$user->name}} </td>
+                    <td>{{$user->name}} </td>
 
-            <td>{{$user->last_name}} </td>
+                    <td>{{$user->last_name}} </td>
 
-            <td>{{$user->email}} </td>
+                    <td>{{$user->email}} </td>
 
-            <td>{{$user->phone}} </td>
+                    <td>{{$user->phone}} </td>
 
-            <td>{{$user->address}} </td>
+                    <td>{{$user->address}} </td>
 
-            <td>{{$user->country}} </td>
+                    <td>{{$user->country}} </td>
 
-            <td><button type="edit" class="btn btn-default">Edit</button> <button type="delete" class="btn btn-default">Delete</button> </td>
+                    <td><button type="edit" class="btn btn-default">Edit</button> <button type="delete" class="btn btn-default">Delete</button> </td>
 
-            <td><a href="{{ route('seePurchaseHistory',['user' => $user]) }}"><button type="purchaseHistory" class="btn btn-default">Purchase History</button> </td>
+                    <td><a href="{{ route('seePurchaseHistory',['user' => $user]) }}"><button type="purchaseHistory" class="btn btn-default">Purchase History</button> </td>
 
-        </tr>
-    @endforeach
+                </tr>
+            @endforeach
 
-    </tbody>
+            </tbody>
+        </table>
 
-</table>
+        <script>
+            function myFunction() {
+                var input, filter, table, tr, td, i;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                table = document.getElementById("myTable");
+                tr = table.getElementsByTagName("tr");
+                for (i = 0; i < tr.length; i++) {
+                    td = tr[i].getElementsByTagName("td")[0];
+                    if (td) {
+                        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                    }
+                }
+            }
+        </script>
+
 @endsection
